3e1223bf9ff7f65a5dc040739c4d2d42636bd777
Kevin Sweeney <sweeney.kj@gmail.com>
2016-05-08 03:08:03 +0800

    migration to flowrouter

---
+++ app/client/home/home.js
@@ -0,0 +1,13 @@
+Template.home.onCreated(function () {
+  this.showing = new ReactiveVar(false);
+});
+
+Template.home.helpers({
+  show: () => Template.instance().showing.get()
+});
+
+Template.home.events({
+  'click button': function (e, t) {
+    t.showing.set(!t.showing.get());
+  }
+});
